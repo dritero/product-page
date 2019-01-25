1fbb1edfae29c3e457cddf2a85c3722b0586fd12
Dritero <driteroavd@gmail.com>
2019-01-25 10:36:45 +0100

    Add image and url with v-bind in attributes

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ var app = new Vue ({
     el: '#app',
     data: {
         product: "Nexus LG",
+        description: "The best Smartphone that has ever existed",
+        image: 'https://s.yimg.com/aah/yhst-77219656498823/lg-nexus-5x-back-battery-cover-replacement-quartz-53.gif',
+        link: 'https://s.yimg.com/aah/yhst-77219656498823/lg-nexus-5x-back-battery-cover-replacement-quartz-53.gif'
     }
 })
