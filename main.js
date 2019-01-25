6725745a137536cb0dedfba7103151e9e706b14b
Dritero <driteroavd@gmail.com>
2019-01-25 10:59:44 +0100

    Add product specifications

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var app = new Vue ({
         description: "The best Smartphone that has ever existed",
         image: 'https://s.yimg.com/aah/yhst-77219656498823/lg-nexus-5x-back-battery-cover-replacement-quartz-53.gif',
         inStock: false,
-        inventory: 7
+        inventory: 7,
+        onSale: false,
+        details: ["Weight: 136g.", "Dimensions: 147 x 72.6 x 7.9mm.", "OS: Android Oreo.", "Screen size: 5.2-inch."]
     }
 })
